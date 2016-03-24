10c856c29a6f7dc015572a1f1d9c9777076bcc55
Guillaume Saint-Raymond <tcherno@gmail.com>
2016-03-24 22:17:55 +0100

    Fix : java.lang.OutOfMemoryError / reducing cache size + recycle

--- app/src/main/java/fr/gstraymond/cache/BitmapCache.java
+++ app/src/main/java/fr/gstraymond/cache/BitmapCache.java
@@ -3,16 +3,30 @@ package fr.gstraymond.cache;
 import android.graphics.Bitmap;
 import android.util.LruCache;
 
+import fr.gstraymond.tools.Log;
+
 public class BitmapCache {
     private LruCache<String, Bitmap> memLruCache;
+    private Log log = new Log(this);
 
     public BitmapCache() {
         final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        final int cacheSize = maxMemory / 3;
+        final int cacheSize = maxMemory / 30;
+        log.d(String.format("BitmapCache --> cachesize %s", cacheSize));
+
         memLruCache = new LruCache<String, Bitmap>(cacheSize) {
             @Override
+            protected void entryRemoved(boolean evicted, String key, Bitmap oldValue, Bitmap newValue) {
+                super.entryRemoved(evicted, key, oldValue, newValue);
+                log.d(String.format("entryRemoved --> recycling %s", key));
+                oldValue.recycle();
+            }
+
+            @Override
             protected int sizeOf(String key, Bitmap bitmap) {
-                return bitmap.getByteCount() / 1024;
+                int size = bitmap.getByteCount() / 1024;
+                log.d(String.format("sizeof --> %s %s", size, key));
+                return size;
             }
         };
     }
