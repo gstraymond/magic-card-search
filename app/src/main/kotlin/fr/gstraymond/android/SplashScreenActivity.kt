d38d6f16a10f1c9d4cbc4efb95c2841483d35790
Guillaume Saint-Raymond <tcherno@gmail.com>
2017-04-05 22:20:23 +0200

    Fixed splashscreen

--- app/src/main/kotlin/fr/gstraymond/android/SplashScreenActivity.kt
+++ app/src/main/kotlin/fr/gstraymond/android/SplashScreenActivity.kt
@@ -1,28 +1,14 @@
 package fr.gstraymond.android
 
 import android.os.Bundle
-import com.magic.card.search.commons.log.Log
-import fr.gstraymond.R
+import android.support.v7.app.AppCompatActivity
 import fr.gstraymond.biz.SearchOptions
 import fr.gstraymond.utils.startActivity
-import java.util.*
 
-class SplashScreenActivity : CustomActivity(R.layout.activity_splash) {
-
-    private val started = Date()
-    private val log = Log(this)
+class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startNextActivity()
-    }
-
-    // FIXME remove me
-    fun startNextActivity() {
-        val remainingTime = 1000 - (Date().time - started.time)
-        log.d("remaining time: $remainingTime")
-        if (remainingTime > 0) Thread.sleep(remainingTime)
-
         startActivity {
             CardListActivity.getIntent(this, SearchOptions(size = 0))
         }
